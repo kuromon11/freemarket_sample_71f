b6b2526e287f55fab6b736ca63af99114be7ac09
kuromon11 <yoshikikuro712@gmail.com>
2020-03-18 11:53:08 +0900

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,10 @@
 lock "~> 3.12.1"
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'freemarket_sampl_71f'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kuromon11/chat-space.git'
+set :repo_url,  'git@github.com:kuromon11/freemarket_sampl_71f.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
