fa4972ed94a3aafc3aab5a6cfb62a10576896823
kuromon11 <yoshikikuro712@gmail.com>
2020-03-23 20:01:03 +0900

    edit images

--- db/schema.rb
+++ db/schema.rb
@@ -48,4 +48,5 @@ ActiveRecord::Schema.define(version: 2020_03_19_032129) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+  add_foreign_key "messages", "users"
 end
