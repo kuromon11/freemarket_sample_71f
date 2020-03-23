6ef1427e7653dc51bbd8a882290a27c318aee303
kuromon11 <yoshikikuro712@gmail.com>
2020-03-23 19:36:18 +0900

    edit images and association

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,9 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   belongs_to :sizes
-  has_many :likes dependent: :destroy
-  has_many :coments dependent: :destroy
-  has_many :messages dependent: :destroy
+  # has_many :messages dependent: :destroy
   has_one :shipping
   has_many :reviews
-  has_many :item_images dependent: :destroy
+  # has_many :item_images dependent: :destroy
   has_one :order
 end
