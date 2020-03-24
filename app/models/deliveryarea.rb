72c8ba5b507bf7e7078580379f81e4756d3d8a24
kuromon11 <yoshikikuro712@gmail.com>
2020-03-24 16:44:03 +0900

    form collection

--- app/models/deliveryarea.rb
+++ app/models/deliveryarea.rb
@@ -1,5 +1,5 @@
 class Deliveryarea < ActiveHash::Base
-  include ActiveHash::Associations
+  # include ActiveHash::Associations
   self.data = [
     { id: 1, name: '北海道' },
     { id: 2, name: '青森県' },
