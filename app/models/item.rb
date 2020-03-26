ff1567750280c662d1b250f5f74a3bfcec443fcf
kuromon11 <yoshikikuro712@gmail.com>
2020-03-26 12:05:07 +0900

    add model category and validate items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  #アソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :deliveryfeeburgen
   belongs_to_active_hash :deliveryservice
@@ -9,18 +10,21 @@ class Item < ApplicationRecord
   belongs_to :category, optional: true 
   belongs_to :brand, optional: true 
   belongs_to :user
-
   has_one :shipping
   accepts_nested_attributes_for :shipping
-  # has_many :reviews
   # has_many :item_images ,dependent: :destroy
   # has_one :order
-  # belongs_to :sizes
-  # has_many :likes ,dependent: :destroy
-  # has_many :coments ,dependent: :destroy
-  # has_many :messages ,dependent: :destroy
 
+  #バリデーション
+  # validates :images, presence: true, length: {manimum: 1, maximum: 4}
   validates :name, presence: true, length: { maximum: 40 }
   validates :i_text, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :fee_burgen_id, presence: true
+  validates :service_id, presence: true
+  validates :area_id, presence: true
+  validates :handling_time_id, presence: true
+  validates :price, presence: true, inclusion: 300..9999999
 
 end
--- db/migrate/20200324033710_remove_categories_from_name.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCategoriesFromName < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :categories, :name
-    remove_reference :categories, :path
-  end
-end
