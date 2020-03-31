9ad6b4eecc6106a5deef590f3817a58aca6830a7
kuromon11 <yoshikikuro712@gmail.com>
2020-03-31 10:36:22 +0900

    edit number

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,19 +1,20 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_79ae2d2743199a76f3eb36aa"); //ここに公開鍵を直書き
+      Payjp.setPublicKey("sk_test_66370c9c8e7b6acb3b00b7e5"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
         e.preventDefault(); //ボタンを一旦無効化します
+        console.log("OK");
         let card = {
-          number: document.getElementById("card_number").value,
+          number: document.getElementById("payment_card_no1").value,
           cvc: document.getElementById("cvc").value,
           exp_month: document.getElementById("exp_month").value,
           exp_year: document.getElementById("exp_year").value
         }; //入力されたデータを取得します。
         Payjp.createToken(card, (status, response) => {
           if (status === 200) { //成功した場合
-            $("#card_number").removeAttr("name");
+            $("#payment_card_no1").removeAttr("name");
             $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
             $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
