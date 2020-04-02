de879d5da94ff3fa576100871542f74867d3807f
kawaguchihitoshi <61231995+kawaguchihitoshi@users.noreply.github.com>
2020-04-02 14:23:41 +0900

    edit2
    edit2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show, :edit,:update,:destroy]
+
   def index
     @items = Item.includes(:item_images).order('created_at DESC')
     @item_images = ItemImage.new
@@ -23,18 +25,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item=Item.find(params[:id])
   end
 
   def edit
-    @item=Item.find(params[:id])
   end
   
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
-    redirect_to("/")
+    redirect_to root_path
   end
 
   def destroy
@@ -44,5 +43,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :i_text, :condition_id, :category_id, :brand_id, :price, shipping_attributes: [:id,:fee_burgen_id, :service_id, :area_id, :handling_time_id], item_images_attributes: [:id,:image_url]).merge(user_id: current_user.id)
   end
-
+  
+  def set_item
+    @item=Item.find(params[:id])
+  end
 end
