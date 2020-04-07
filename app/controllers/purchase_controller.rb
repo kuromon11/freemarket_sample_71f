24b32f8b9d608cad8cfe2707f4aa87bf078a3f96
masaaki-kamegai <gbmc.0301@gmail.com>
2020-04-07 17:46:08 +0900

    fix for purchasecontroller

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -27,7 +27,7 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = 'sk_test_66370c9c8e7b6acb3b00b7e5'
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
